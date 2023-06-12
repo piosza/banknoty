3e2e0cbe20279c162c9111bcdfa776b25005f7da
Piosza <piosza@tlen.pl>
2023-06-12 20:03:32 +0200

    new logic of worlf new order

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print("   $$$$$$$$$$$$$$$$$$$$$$    ")
 print(" ite to banknotow i monet ")
 print("policzmy ile to banknotow i monet")
 print("banknoty to, banknoty")
-print("podaj kwote")
+print("$$$$$$$$$$$$$$$$$4")
 
 
 banknoty = [
@@ -14,36 +14,20 @@ banknoty = [
     10,
     5,
     2,
-    500,
-    200,
-    100,
-    50,
-    20,
-    10,
-    5,
-    2,
-    500,
-    200,
-    100,
-    50,
-    20,
-    10,
-    5,
-    2,
     1,
 ]
-banknoty.sort()
-banknoty.reverse()
 
 a = int(input("podaj kwote "))
 waluta = a
 kasa = []
 i = 0
 
-while i < len(banknoty) and (waluta - sum(kasa) >= min(banknoty)):
-    if (waluta - sum(kasa)) >= banknoty[i]:
+while sum(kasa) < a:
+    ilosc_bankonotow = waluta // banknoty[i]
+    (waluta) %= banknoty[i]
+    for j in range(ilosc_bankonotow):
         kasa.append(banknoty[i])
     i += 1
 
 print(" suma : ", sum(kasa))
-print("banknoty : ", kasa)
+print("banknoty i monety : ", kasa)
